65a2649775a018ec95456ea163a44878512dd409
jadubhai3669-lang <jadubhai3669@gmail.com>
2026-04-13 16:54:31 +0530

    SEO added

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import sitemap from 'vite-plugin-sitemap'
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss(), viteSingleFile(),sitemap({ hostname: 'https://ravindrayadav.online' })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
